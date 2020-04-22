9dc168fe5972f82cea802a2fdcaf4d7acf4302b0
Sikiru Olatunji <Sikiru.Olatunji@wemabank.com>
2020-04-22 19:00:31 +0100

    Add project files.

---
+++ CakeShop/Controllers/PieController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CakeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CakeShop.Controllers
+{
+	public class PieController : Controller
+	{
+
+		private readonly IPieRepository _pieRepository;
+		private readonly ICategoryRepository _categoryRepository;
+
+		public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+		{
+			_pieRepository = pieRepository;
+			_categoryRepository = categoryRepository;
+		}
+
+		public ViewResult List()
+		{
+			return View(_pieRepository.AllPies);
+		}
+	}
+}
---
+++ CakeShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CakeShop.Models
+{
+	//This is implementing category Repository interface
+	public class MockCategoryRepository: ICategoryRepository
+	{
+		public IEnumerable<Category> AllCategories => new List<Category>
+		{
+			new Category{CategoryId = 1, CategoryName = "Fruit Pies", Description = "All-fruity Pies"},
+			new Category{CategoryId = 2, CategoryName = "Cheese Cake", Description = "Cheesy all the way"},
+			new Category{CategoryId = 3, CategoryName = "Seasonal Pie", Description = "Get in all the mood for a seasonal pie."}
+		};
+	}
+}
