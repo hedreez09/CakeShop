6830119db7d02968182ec538c12b27cb82eb7a76
Idris.Wonuola <Idris.Wonuola@wemabank.com>
2020-05-01 09:55:22 +0100

    Creation of Partial view for easy navigation

--- CakeShop/Controllers/HomeController.cs
+++ CakeShop/Controllers/HomeController.cs
@@ -6,32 +6,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CakeShop.Models;
+using CakeShop.ViewModels;
 
 namespace CakeShop.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+		private readonly IPieRepository _pieRepository;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(IPieRepository pieRepository)
 		{
-			_logger = logger;
+			_pieRepository = pieRepository;
 		}
 
 		public IActionResult Index()
 		{
-			return View();
-		}
-
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+			var homeViewModel = new HomeViewModel
+			{
+				PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+			};
+			return View(homeViewModel);
 		}
 	}
 }
